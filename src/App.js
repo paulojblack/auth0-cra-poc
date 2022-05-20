f962810804188bcaa0ce22d6627dc9fa557c701d
Paulo Black <pblack@goodwaygroup.com>
2022-05-20 15:42:15 -0400

    login/logout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
+import LoginButton from './Login';
+import Logout from './Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoginButton></LoginButton>
+      <Logout></Logout>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { Auth0Provider } from "@auth0/auth0-react";
+console.log(window.location.origin)
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-rtvrxdr3.us.auth0.com"
+    clientId="FMGQhsIy7MDFMoPSLpLwdsD8ioqN2hKV"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
